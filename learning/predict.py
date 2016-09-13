bfc2028fa1c2bc7b801a4b571b4e6a60c5b19800
Tom Herold <heroldtom@gmail.com>
2016-09-13 16:15:47 +0200

    finished prediction script

--- learning/predict.py
+++ learning/predict.py
@@ -1,13 +1,11 @@
 from __future__ import division
 
 import os.path
-import time
-from datetime import datetime
 import subprocess
 import glob
+import shutil
 
 import numpy as np
-from sklearn.metrics import accuracy_score, classification_report, precision_recall_fscore_support, confusion_matrix
 from yaml import load
 
 import sound_loader
@@ -38,7 +36,6 @@ def file_to_segments():
         )
 
         command = ["ffmpeg", "-y", "-i", file_name, "-map", "0", "-ac", "1", "-ar", "16000", "-f", "segment", "-segment_time", "10", output_file_name]
-        print
         subprocess.call(command)
 
         file_pattern = os.path.join(temp_directory, "*.wav")
@@ -50,43 +47,34 @@ def file_to_segments():
         raise Exception("-file argument is not a valid file: {}".format(file_name))
 
 
+def audio_to_spectrogram(file_name):
+
+    spectrogram = sound_loader.wav_to_spectrogram(file_name)
+
+    image = spectrogram - 0.5
+    image = image * 2.0
+
+    return image
+
+
 def predict():
     """Predict class for a new audio file using a trained model."""
 
     with tf.Graph().as_default():
 
+        # Load file from disk and convert to spectrograms
         file_list = file_to_segments()
         config["batch_size"] = len(file_list)
 
-        filename_queue = tf.train.string_input_producer(file_list)
-        images = []
+        image_data = map(audio_to_spectrogram, file_list)
 
-        for i in range(config["batch_size"]):
-
-            single_segment_filename = filename_queue.dequeue()
-            image = tf.py_func(sound_loader.wav_to_spectrogram, [single_segment_filename], [tf.double])[0]
-
-            image = tf.image.convert_image_dtype(image, dtype=tf.float32)
-            image_shape = [config["image_height"], config["image_width"], config["image_depth"]]
-            image.set_shape(image_shape)  # TODO Which way around is it?
-
-            # Finally, rescale to [-1,1] instead of [0, 1)
-            image = tf.sub(image, 0.5)
-            image = tf.mul(image, 2.0)
-
-            images.append(image)
-
-        image_batch = tf.train.batch(
-            images,
-            batch_size=config["batch_size"],
-            capacity=config["batch_size"],
-        )
+        image_shape = [config["image_height"], config["image_width"], config["image_depth"]]
+        image_batch = tf.placeholder(dtype=tf.float32, shape=[config["batch_size"]] + image_shape)
 
         # Init Model
         logits = crnn_model.inference(image_batch, config)
         # Use the last state of the LSTM as output
         predictions_op = tf.nn.softmax(logits[-1])
-        label_op = tf.reduce_mean(tf.argmax(predictions_op, dimension=1))
 
         sess = tf.Session()
         init = tf.initialize_all_variables()
@@ -111,31 +99,21 @@ def predict():
             print("No checkpoint file found")
             return
 
+        # Returns probabilities for every segment
+        probability = sess.run(predictions_op, feed_dict={image_batch: image_data})
 
-        # Start the queue runners.
-        coord = tf.train.Coordinator()
-        with coord.stop_on_exception():
-            try:
-                threads = []
-                for qr in tf.get_collection(tf.GraphKeys.QUEUE_RUNNERS):
-                    threads.extend(qr.create_threads(sess, coord=coord, daemon=True, start=True))
+        # Use median, so outlies don't interfere as much
+        median_probabilies = np.median(probability, axis=0)
+        label_index = np.argmax(median_probabilies)
+        label = config["label_names"][label_index]
 
-                print("%s: starting evaluation on (%s)." % (datetime.now(), config["test_data_dir"],))
-                step = 0
-                predicted_labels = np.array([])
+        print("Probability raw", probability)
+        print("Probability: ", median_probabilies)
+        print("Label: ", label, label_index)
 
+        shutil.rmtree(temp_directory, ignore_errors=True)
 
-                probability, label = sess.run([predictions_op, label_op])
-                print probability, label
-
-
-            except Exception as e:
-                coord.request_stop(e)
-
-            coord.request_stop()
-            coord.join(threads, stop_grace_period_secs=10)
-
-            os.rmdir(temp_directory)
+        return probability, label
 
 
 if __name__ == "__main__":
