fd9fb23170a81e5c66dc23ed7d8dadaa8458e68e
Tom Herold <tom.herold@scalableminds.com>
2016-12-05 16:47:34 +0100

    added test split to create_csv

--- data/create_csv.py
+++ data/create_csv.py
@@ -26,7 +26,7 @@ def get_immediate_subdirectories(path):
     return [name for name in os.listdir(path) if os.path.isdir(os.path.join(path, name))]
 
 
-def create_csv(root_dir, train_test_split=0.8):
+def create_csv(root_dir, train_validation_split=0.8):
     languages = get_immediate_subdirectories(root_dir)
     counter = {}
     file_names = {}
@@ -45,24 +45,30 @@ def create_csv(root_dir, train_test_split=0.8):
     print(counter)
     smallest_count = min(counter.values())
 
-    num_train = int(smallest_count * train_test_split)
-    num_validation = smallest_count - num_train
+    num_test = int(smallest_count * 0.1)
+    num_train = int(smallest_count * train_validation_split - 0.1)
+    num_validation = smallest_count - num_train - num_test
+
 
     # Split datasets and shuffle languages
     training_set = []
     validation_set = []
+    test_set = []
 
     for lang in languages:
         label = LABELS[lang]
         training_set += zip(file_names[lang][:num_train], itertools.repeat(label))
         validation_set += zip(file_names[lang][num_train:num_train + num_validation], itertools.repeat(label))
+        test_set += zip(file_names[lang][num_train + num_validation:num_train + num_validation + num_test], itertools.repeat(label))
 
     shuffle(training_set)
     shuffle(validation_set)
+    shuffle(test_set)
 
     # Write to CSV
     train_file = open(os.path.join(root_dir, "training.csv"), "w")
     validation_file = open(os.path.join(root_dir, "validation.csv"), "w")
+    test_file = open(os.path.join(root_dir, "testing.csv"), "w")
 
     for (filename, label) in training_set:
         train_file.write("{}, {}\n".format(filename, label))
@@ -70,18 +76,23 @@ def create_csv(root_dir, train_test_split=0.8):
     for (filename, label) in validation_set:
         validation_file.write("{}, {}\n".format(filename, label))
 
+    for (filename, label) in test_set:
+        test_file.write("{}, {}\n".format(filename, label))
+
     train_file.close()
     validation_file.close()
+    test_file.close()
 
     # Stats
     print("[Training]   Files per language: {} Total: {}".format(num_train, num_train * len(languages)))
     print("[Validation] Files per language: {} Total: {}".format(num_validation, num_validation * len(languages)))
+    print("[Testing]    Files per language: {} Total: {}".format(num_test, num_test * len(languages)))
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--dir', dest='root_dir', default=os.getcwd())
-    parser.add_argument('--split', dest='train_test_split', default=0.8)
+    parser.add_argument('--split', dest='train_validation_split', default=0.8)
     args = parser.parse_args()
 
-    create_csv(args.root_dir, args.train_test_split)
+    create_csv(args.root_dir, args.train_validation_split)
