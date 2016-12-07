bec2a53bf80f73f6a051f7641854c26585d9a99d
MPSS2015 <mpss2015@mpss2015.de>
2016-12-07 15:51:36 +0100

    reset pooling shape fro topcoder CRNN

--- keras/models/topcoder_crnn.py
+++ keras/models/topcoder_crnn.py
@@ -17,23 +17,23 @@ def create_model(input_shape, config, is_training=True):
 
     model.add(Convolution2D(16, 7, 7, W_regularizer=l2(weight_decay), activation="relu", input_shape=input_shape))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 1), strides=(2, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     model.add(Convolution2D(32, 5, 5, W_regularizer=l2(weight_decay), activation="relu"))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 1), strides=(2, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     model.add(Convolution2D(64, 3, 3, W_regularizer=l2(weight_decay), activation="relu"))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 1), strides=(2, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     model.add(Convolution2D(128, 3, 3, W_regularizer=l2(weight_decay), activation="relu"))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 1), strides=(2, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     model.add(Convolution2D(256, 3, 3, W_regularizer=l2(weight_decay), activation="relu"))
     model.add(BatchNormalization())
-    model.add(MaxPooling2D(pool_size=(2, 1), strides=(2, 1)))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
 
     # (bs, y, x, c) --> (bs, x, y, c)
     model.add(Permute((2, 1, 3)))
